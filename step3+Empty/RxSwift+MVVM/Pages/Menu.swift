f90f75d8211680f54c47b76435286d96243a6f20
Zero <joyh0808@mgail.com>
2024-01-24 11:41:05 +0900

    ViewModel Menu 추가

---
+++ step3+Empty/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by 조영현 on 1/24/24.
+//  Copyright © 2024 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// ViewModel
+struct Menu {
+    var naem: String
+    var price: Int
+    var count: Int
+}
